922bbddbcb0da9bd5937a24e9a57f795c1b2f323
xparkerx317 <59152856+xparkerx317@users.noreply.github.com>
2019-12-25 16:40:50 +0800

    Revert "20191225 V1.0"
    This reverts commit c8c0d6402c3ff72b8e95e6e6ce9a14667841122d.

--- happy_cake_6/backend/layouts/head.php
+++
@@ -1,4 +0,0 @@
-<meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="../css/font-awesome.min.css" type="text/css">
-  <link rel="stylesheet" href="../css/theme.css" type="text/css">
--- happy_cake_6/backend/news/delete.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once('../is_login.php');
-require_once('../../function/connection.php');
-$query = $db->query("DELETE FROM news WHERE newsID=".$_GET['newsID']);
-header('Location: list.php');
-?>
