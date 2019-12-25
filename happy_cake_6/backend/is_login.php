ca4f4cc6a2f388937ef016f27361f785441e1a8e
xparkerx317 <59152856+xparkerx317@users.noreply.github.com>
2019-12-25 16:34:38 +0800

    Revert "20191225 V1.0"
    This reverts commit c8c0d6402c3ff72b8e95e6e6ce9a14667841122d.

--- happy_cake_6/backend/is_login.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-if(!isset($_SESSION['user']) && $_SESSION['user']['account'] == null){
-    header('Location: ../login.php?MSG=please_login');
-}
-
-?>
--- happy_cake_6/frontend/cart/cart_delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-$index = $_GET['CartID'];
-unset($_SESSION['Cart'][$index]);
-$_SESSION['Cart'] = array_values($_SESSION['Cart']);
-
-header('Location: ../basket.php?Del=true');
-?>
